3cf422f4b827f75647ae2fa6467346dd2b95a218
Dmitry Zavyalov <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-08-29 19:51:30 +0300

    Add CLI and env options

--- main.py
+++ main.py
@@ -1,26 +1,50 @@
 import asyncio
 import datetime
+import os
+import pathlib
 
 import aiofiles
+import configargparse
+from environs import env
 
 
-async def tcp_echo_client(message):
-    while True:
-        reader, writer = await asyncio.open_connection(
-            'minechat.dvmn.org', 5000)
+async def tcp_echo_client(message, host, port, log_path):
+    try:
+        reader, writer = await asyncio.open_connection(host, port)
+        print('Соединение установлено.')
+        async with aiofiles.open(log_path, 'a', encoding='utf-8') as f:
+            await f.write(f'[{datetime.datetime.now()}] Соединение установлено. \n')
+        while True:
+            # print(f'Send: {message!r}')
+            # writer.write(message.encode())
+            # await writer.drain()
 
-        # print(f'Send: {message!r}')
-        # writer.write(message.encode())
-        # await writer.drain()
+            data = await reader.read(100)
+            print(f'[{datetime.datetime.now()}] {data.decode()}')
 
-        data = await reader.read(100)
-        print(f'[{datetime.datetime.now()}] {data.decode()}')
+            # print('Close the connection')
+            # writer.close()
+            # await writer.wait_closed()
+            async with aiofiles.open(log_path, 'a', encoding='utf-8') as f:
+                await f.write(f'[{datetime.datetime.now()}] {data.decode()} \n')
+    except Exception as e:
+        print('ОШИБКА! Соединение прервано.')
+        async with aiofiles.open(log_path, 'a', encoding='utf-8') as f:
+            await f.write(f'[{datetime.datetime.now()}] ОШИБКА! Соединение прервано. \n')
+        asyncio.run(tcp_echo_client(message, host, port, log_path))
 
-        # print('Close the connection')
-        # writer.close()
-        # await writer.wait_closed()
-        async with aiofiles.open('message_history.txt', 'a', encoding='utf-8') as f:
-            await f.write(f'[{datetime.datetime.now()}] {data.decode()} \n')
 
+if __name__ == '__main__':
+    env.read_env()
 
-asyncio.run(tcp_echo_client('Hello World!'))
+    parser = configargparse.ArgumentParser()
+    parser.add_argument('--port', env_var='PORT')
+    parser.add_argument('--host', env_var='HOST')
+    parser.add_argument('--log_path', default='message_history.txt', type=str, env_var='LOG_PATH', )
+    args = parser.parse_args()
+
+    port = args.port
+    host = args.host
+    log_path = args.log_path
+
+    asyncio.run(tcp_echo_client('Hello World!', host, port, log_path))
